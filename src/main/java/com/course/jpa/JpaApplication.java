2f6643873ebcd720afb2d605d251fd346d643788
Franco-Spinelli <spinellifranco04@gmail.com>
2024-06-06 18:21:25 -0300

    Use @Transactional and update Person

--- src/main/java/com/course/jpa/JpaApplication.java
+++ src/main/java/com/course/jpa/JpaApplication.java
@@ -6,24 +6,62 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class JpaApplication implements CommandLineRunner {
+	@Autowired
+	private IPersonRepository personRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
 	}
 
-	@Autowired
-	private IPersonRepository personRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
+
+		findOne();
+
+	}
+
+	public void findOne(){
+		Person person = null;
+		Optional<Person>optionalPerson = personRepository.findOneName("Andres");
+		if(optionalPerson.isPresent()){
+			person = optionalPerson.get();
+			System.out.println(person.getName());
+		}
+	}
+	@Transactional
+	public void create(){
+		Person person = new Person(null,"Myke","Street","C");
+		personRepository.save(person);
+	}
+	@Transactional
+	public void update(){
+		Person person = null;
+		Optional<Person>optionalPerson = personRepository.findById(1L);
+		if(optionalPerson.isPresent()){
+			person = optionalPerson.get();
+			person.setProgrammingLanguage("C");
+			Person personDb = personRepository.save(person);
+			System.out.println(personDb);
+		}
+	}
+	public void list(){
 		//List<Person>persons = (List<Person>) personRepository.byProgrammingLanguage("Java");
 		List<Person>persons = (List<Person>) personRepository.findByProgrammingLanguageAndName("Java","Andres");
 		persons.stream().forEach(person -> {
 			System.out.println(person.getName());
+		});
+
+		List<Object[]>data = personRepository.getPersonData("Andres");
+		data.stream().forEach(person -> {
+			System.out.println(person[0] + " is expert in " + person[1]);
 		});
 	}
 }
--- src/main/java/com/course/jpa/repositories/IPersonRepository.java
+++ src/main/java/com/course/jpa/repositories/IPersonRepository.java
@@ -5,8 +5,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 public interface IPersonRepository extends CrudRepository<Person,Long> {
+    @Query("select p from Person p where p.id=?1")
+    Optional<Person>findOne(Long id);
+    @Query("select p from Person p where p.name=?1")
+    Optional<Person>findOneName(String name);
+    @Query("select p from Person p where p.name like %?1%")
+    Optional<Person>findOneLikeName(String name);
+    Optional<Person>findByNameContaining(String name);
     //Find by programming language using @QueryMethod
     List<Person>findByProgrammingLanguage(String programmingLanguage);
 
@@ -16,5 +25,11 @@ public interface IPersonRepository extends CrudRepository<Person,Long> {
     //Find by programming language using @Query
     @Query("select p from Person p where p.programmingLanguage=?1")
     List<Person>byProgrammingLanguage(String programmingLanguage);
+    @Query("select p.name, p.programmingLanguage from Person p")
+    List<Object[]> getPersonData();
+    @Query("select p.name, p.programmingLanguage from Person p where p.name=?1")
+    List<Object[]> getPersonData(String name);
+    @Query("select p.name, p.programmingLanguage from Person p where p.name=?1 and p.programmingLanguage=?2")
+    List<Object[]> getPersonData(String name, String programmingLanguage);
 }
 
